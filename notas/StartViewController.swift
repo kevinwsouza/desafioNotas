8d849a09596bb3815fd955e0886bbe6a6989b5b2
Kevin Souza <60259161+kevinwsouza@users.noreply.github.com>
2021-03-04 17:59:23 -0300

    commit desafio 2

---
+++ notas/StartViewController.swift
@@ -0,0 +1,23 @@
+//
+//  StartViewController.swift
+//  notas
+//
+//  Created by Kevin willian Jorge souza on 04/03/21.
+//
+
+import UIKit
+
+class StartViewController: UIViewController {
+
+    
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
