aae73ebe407d6271babd0c2d5d0d27c8446e9932
Kevin Souza <60259161+kevinwsouza@users.noreply.github.com>
2021-03-05 16:20:25 -0300

    1.1v

--- notas/StartViewController.swift
+++ notas/StartViewController.swift
@@ -9,7 +9,21 @@ import UIKit
 
 class StartViewController: UIViewController {
 
+    @IBOutlet weak var lbErro: UILabel!
+    @IBOutlet weak var tfNameStudent: UITextField!
+    @IBOutlet weak var tfGradeStudent: UITextField!
     
+    var student = Student(nameStudent: "", scholarGrade: 0, gradeArray: [])
+    
+    @IBAction func btSignIn(_ sender: Any) {
+        if String(tfNameStudent.text!) != " ", Int(tfGradeStudent.text!) != nil{
+            performSegue(withIdentifier: "segueGrades", sender: nil)
+            
+        } else {
+            let erro = "Insira Nome de aluno ou Série Válidos"
+            lbErro.text = "\(erro)"
+        }
+    }
     
     
     
--- notas/ViewController.swift
+++ notas/ViewController.swift
@@ -12,9 +12,10 @@ class ViewController: UIViewController {
     @IBOutlet weak var lbSTrimestre: UITextField!
     @IBOutlet weak var lbTTrimestre: UITextField!
     @IBOutlet weak var lbconcept: UILabel!
+    @IBOutlet weak var lbStudentName: UILabel!
     
     var grade: Float = 0
-    
+    var student = Student(nameStudent: "", scholarGrade: 0, gradeArray: [])
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         view.endEditing(true)
@@ -27,7 +28,15 @@ class ViewController: UIViewController {
             if ((primeiro >= 0 && primeiro <= 10) && (segundo >= 0 && segundo <= 10) && (terceiro >= 0 && terceiro <= 10)){
               
                 
-                grade = (primeiro + segundo + terceiro) / 3.0
+                student.gradeArray[0] = primeiro
+                student.gradeArray[1] = segundo
+                student.gradeArray[2] = terceiro
+                
+                func sumArray(array:Float) -> Float {
+                    let sumArray = (student.gradeArray[0]) + (student.gradeArray[1]) + (student.gradeArray[2])
+                }
+                
+                grade = sumArray / 3.0
                 showResult()
             }else{
                 let erro = "Insira uma nota entre 0 e 10"
@@ -48,6 +57,8 @@ class ViewController: UIViewController {
         default:
             text = "Aprovado"
         }
+        lbStudentName.text = "O" + "\(String(student.nameStudent))" + "está"
+        
         lbconcept.text = "\(String(format: "%.2f", grade)) : \(text)"
     }
 
